1516cbff553e887428e24069c47e621c964ccdac
zackmeath <zack.meath@gmail.com>
2015-09-14 22:23:48 -0400

    Alters ver command, closes #1

--- distrib/globals.js
+++ distrib/globals.js
@@ -11,7 +11,7 @@
 // Global CONSTANTS (TypeScript 1.5 introduced const. Very cool.)
 //
 var APP_NAME = "zmOS"; // 'cause Bob and I were at a loss for a better name.
-var APP_VERSION = "0.00"; // What did you expect?
+var APP_VERSION = "0.01"; // What did you expect?
 var CPU_CLOCK_INTERVAL = 100; // This is in ms (milliseconds) so 1000 = 1 second.
 var TIMER_IRQ = 0; // Pages 23 (timer), 9 (interrupts), and 561 (interrupt priority).
 // NOTE: The timer is different from hardware/host clock pulses. Don't confuse these.
--- source/globals.ts
+++ source/globals.ts
@@ -11,7 +11,7 @@
 // Global CONSTANTS (TypeScript 1.5 introduced const. Very cool.)
 //
 const APP_NAME: string    = "zmOS";   // 'cause Bob and I were at a loss for a better name.
-const APP_VERSION: string = "0.00";   // What did you expect?
+const APP_VERSION: string = "0.01";   // What did you expect?
 
 const CPU_CLOCK_INTERVAL: number = 100;   // This is in ms (milliseconds) so 1000 = 1 second.
 
